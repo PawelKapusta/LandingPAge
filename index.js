1cde2a713f09e87b702f1451550c3feb346096d9
Paweł Kapusta <pawelkapusta70@gmail.com>
2020-08-04 21:23:18 +0200

    Add clock to header

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ setInterval(() => {
     let day = new Date();
     let hh = day.getHours() * 30;
 
-    let mm = day.getMinutes() * deg;
-    let ss = day.getSeconds() * deg;
-    let count = hh + mm / 12;
-    hr.style.transform = "rotateZ(count deg)";
-    mn.style.transform = "rotateZ(mm deg)";
-    sc.style.transform = "rotateZ($(ss) deg)";
+    var mm = day.getMinutes() * deg;
+    var ss = day.getSeconds() * deg;
+    var count = hh + mm / 12;
+    hr.style.transform = "rotateZ(${count}deg)";
+    mn.style.transform = "rotateZ(${mn}deg)";
+    sc.style.transform = "rotateZ(${ss}deg)";
 });
